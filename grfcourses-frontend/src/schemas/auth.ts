17f137a24885948b8093fc320cab3772b01e290f
JuliaFSBarbosa <juliafrancinisimon@gmail.com>
2025-10-13 20:09:05 -0300

    deletados

--- grfcourses-frontend/src/schemas/auth.ts
+++
@@ -1,8 +0,0 @@
-import {z} from 'zod'
-
-export const loginSchema = z.object({
-    email: z.string({ required_error: 'O email é obrigatório.' }).email("Email inválido"),
-    password: z.string({required_error: 'A senha é obrigatória.'}).min(8, "Senha deve ter pelo menos 8 caracteres.")
-})
-
-export type SignInForm = z.infer<typeof loginSchema>
--- grfcourses-frontend/src/services/auth.ts
+++
@@ -1,9 +0,0 @@
-import { SignInForm } from "@/schemas/auth";
-
-export const signIn = async(data: SignInForm) => {
-    return api<APISignInResponse>({
-        endpoint: "/accounts/signin/",
-        method: "POST",
-        data
-    })
-}
