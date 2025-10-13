c73dfaae6ab379f4e64996e1760f89a0b2635deb
JuliaFSBarbosa <juliafrancinisimon@gmail.com>
2025-10-13 20:36:36 -0300

    rename

--- logistic-backend/core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/v1/accounts/', include('accounts.urls')), 
-    path('api/v1/logistics/', include('logistics.urls')),
-]
-    
